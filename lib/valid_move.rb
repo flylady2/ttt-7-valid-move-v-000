a873e22e27cfe14ae2ef0c95418e1d3405785cc4
Leslie Stevens <flylady@mail.utexas.edu>
2018-03-13 20:43:08 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -9,7 +9,7 @@ def position_taken?(board, index)
   else
     nil
   end
-def valid_move(board, index)
+def valid_move?(board, index)
   if index.between?(0, 8) && position_taken?(board, index) == false
     true
   end
